cf67065bcfdbba76632bf1cbd1e09d94f1288103
zephirefaith <priyam8parashar@gmail.com>
2017-08-10 16:31:48 -0700

    prettified

--- index.php
+++ index.php
@@ -5,52 +5,61 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
 <html>
-	<head>
-		<title>Priyam Parashar</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
-		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
-		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
-	</head>
-	<body>
 
-		<!-- Wrapper -->
-			<div id="wrapper">
+<head>
+	<title>Priyam Parashar</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+	<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+</head>
 
-				<!-- Main -->
-					<div id="main">
-						<div class="inner">
+<body>
 
-							<!-- Header -->
-							<?php include("header.html"); ?>
+	<!-- Wrapper -->
+	<div id="wrapper">
 
-							<!-- Banner -->
-								<section id="banner">
-									<div class="content">
-										<header>
-											<h2>About Me</h2>
-										</header>
-										<p>I am a second year graduate student at <i>University of California, San Diego</i>, studying for my <b>PhD in Computer Science with an emphasis on Robotics</b>. My research interests lie in creating a <b>social architecture for robots</b> which supports <b>Human Robot Interaction</b> and <b>long-term development and survival of autonomous mobile robots</b>. My research mainly focuses on creating <b>socially sensitive knowledge-based agents</b> and integrating them with explainable machine learning, with a secondary interest in using cloud as an informational database. I am being co-advised by <b>Dr Henrik Christensen</b> and <b>Dr Ashok Goel</b>, from Georgia Tech.</p>
+		<!-- Main -->
+		<div id="main">
+			<div class="inner">
 
-									</div>
-									<span class="image object">
+				<!-- Header -->
+				<?php include("header.html"); ?>
+
+				<!-- Banner -->
+				<section id="banner">
+					<div class="content">
+						<header>
+							<h2>About Me</h2>
+						</header>
+						<p>I am a second year graduate student at <i>University of California, San Diego</i>, studying for my <b>PhD in Computer Science with an emphasis on Robotics</b>.
+							My research interests lie in creating a <b>social architecture for robots</b> which supports <b>Human Robot Interaction</b>							and <b>long-term development and survival of autonomous mobile robots</b>. My research mainly focuses on creating
+							<b>socially sensitive knowledge-based agents</b> and integrating them with explainable machine learning, with a secondary
+							interest in using cloud as an informational database. I am being co-advised by <b>Dr Henrik Christensen</b> and <b>Dr Ashok Goel</b>,
+							from Georgia Tech.</p>
+
+					</div>
+					<span class="image object">
 										<img src="images/my_image.jpg" alt="" />
 									</span>
-								</section>
+				</section>
 
-								<section>
-									<h2>Education</h2>
-									<p>Before UC San Diego, I spent a year at <i>Georgia Institute of Technology</i> as a PhD student. Prior to which I studied at <i>Robotics Institute, Carnegie Mellon University</i> for my MS in Robotics, graduating in 2014. My undegraduate studies were at <i>International Institute of Information Technology, Hyderabad</i> with a major in Electronics and Communication and minor in Mobile Robotics.</p>
-									<!-- <ul class="actions">
+				<section>
+					<h2>Education</h2>
+					<p>Before UC San Diego, I spent a year at <i>Georgia Institute of Technology</i> as a PhD student. Prior to which I studied
+						at <i>Robotics Institute, Carnegie Mellon University</i> for my MS in Robotics, graduating in 2014. My undegraduate
+						studies were at <i>International Institute of Information Technology, Hyderabad</i> with a major in Electronics and
+						Communication and minor in Mobile Robotics.</p>
+					<!-- <ul class="actions">
 										<li><a href="#" class="button big">Learn More</a></li>
 									</ul> -->
-								</section>
+				</section>
 
-						</div>
-					</div>
+			</div>
+		</div>
 
 		<!--Sidebar-->
 
@@ -59,5 +68,6 @@
 		<!-- Scripts -->
 		<?php include("scripts.html"); ?>
 
-	</body>
+</body>
+
 </html>
